cbdfa8ad318493ba64bf730eeb5d5668b933d0a4
Khaya Mathebula <khaya.mathebula@absa.co.za>
2018-02-13 22:45:25 +0200

    created producer of message and sent to rabbitmq instance running locally

--- spring-cloud-stream/producer/src/main/java/com/example/producer/ProducerApplication.java
+++ spring-cloud-stream/producer/src/main/java/com/example/producer/ProducerApplication.java
@@ -2,11 +2,16 @@ package com.example.producer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.stream.annotation.EnableBinding;
+import org.springframework.cloud.stream.annotation.Output;
+import org.springframework.messaging.MessageChannel;
 
 @SpringBootApplication
+@EnableBinding(ChannelsProducer.class)
 public class ProducerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ProducerApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ProducerApplication.class, args);
+    }
+
 }
