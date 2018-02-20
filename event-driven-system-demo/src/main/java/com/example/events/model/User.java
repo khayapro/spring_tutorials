a7a8e0d93a0e87a1ba4a4c41d4fb84eaf35a88ee
Khaya Mathebula <khaya.mathebula@absa.co.za>
2018-02-20 18:48:42 +0200

    created a Marker Interface for my events so I can have a list of changes in the user object

--- event-driven-system-demo/src/main/java/com/example/events/model/User.java
+++ event-driven-system-demo/src/main/java/com/example/events/model/User.java
@@ -1,29 +1,61 @@
 package com.example.events.model;
 
+import com.example.events.events.DomainEvent;
+import com.example.events.events.UserActivated;
+import com.example.events.events.UserDeactivated;
+import com.example.events.events.UserNameChange;
+
+import java.time.Instant;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import java.util.UUID;
 
 public class User {
 
+
+    public void flushChanges() {
+        this.changes.clear();
+    }
+
+    enum UserState { ACTIVATED, DEACTIVATED }
+
     private UUID id;
     private String name;
     private UserState state;
+    private List<DomainEvent> changes = new ArrayList<>();
 
-    enum UserState { ACTIVATED, DEACTIVATED }
 
     public User(UUID id) {
         this.id = id;
     }
 
+
+    public UUID getId() {
+        return id;
+    }
+
     public void activate(){
         if(isActivated())
             throw new IllegalArgumentException("User already activated.");
+        userActivated(new UserActivated(Instant.now()));
+    }
+
+    private void userActivated(UserActivated activated) {
         this.state = UserState.ACTIVATED;
+        changes.add(activated);
     }
 
     public void deactivate(){
         if(isDeactivated())
             throw new IllegalArgumentException("User already deactivated.");
+        //event
+        userDeactivated(new UserDeactivated(Instant.now()));
+    }
+
+    private void userDeactivated(UserDeactivated deactivated) {
         this.state = UserState.DEACTIVATED;
+        changes.add(deactivated);
     }
 
     public boolean isActivated(){
@@ -38,11 +70,20 @@ public class User {
     public void changeName(final String newName){
         if(isDeactivated())
             throw new IllegalArgumentException("Cannot change name, user deactivated.");
-        this.name = newName;
+        userNameChanged(new UserNameChange(newName, Instant.now()));
+    }
+
+    private void userNameChanged(UserNameChange userNameChange) {
+        this.name = userNameChange.getNewName();
+        changes.add(userNameChange);
     }
 
     public String getName(){
         return name;
+    }
+
+    public List<DomainEvent> getChanges() {
+        return Collections.unmodifiableList(changes);
     }
 
 }
