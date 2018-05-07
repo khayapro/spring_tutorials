d9a6587e34ecd22830860e2dadf75e9252668e6d
Khaya Mathebula <khaya.mathebula@absa.co.za>
2018-05-07 22:30:16 +0200

    establishing channel and message handler to handle message received in that channel

--- spring-integration/src/main/java/com/example/springintegration/SpringIntegrationApplication.java
+++ spring-integration/src/main/java/com/example/springintegration/SpringIntegrationApplication.java
@@ -1,13 +1,17 @@
 package com.example.springintegration;
 
 import com.example.springintegration.services.PrintService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.ImportResource;
+import org.springframework.integration.channel.DirectChannel;
 import org.springframework.messaging.Message;
+import org.springframework.messaging.MessageHandler;
+import org.springframework.messaging.MessagingException;
 import org.springframework.messaging.support.MessageBuilder;
 
 @SpringBootApplication
@@ -16,16 +20,28 @@ import org.springframework.messaging.support.MessageBuilder;
 public class SpringIntegrationApplication implements ApplicationRunner {
 
 
+	@Autowired
+	private DirectChannel channel;
+
 	public static void main(String[] args) {
 		SpringApplication.run(SpringIntegrationApplication.class, args);
 	}
 
 	@Override
 	public void run(ApplicationArguments args) throws Exception {
+
+		//to handle message sent to this channel
+		channel.subscribe(new MessageHandler() {
+			@Override
+			public void handleMessage(Message<?> message) throws MessagingException {
+				new PrintService().print((Message<String>)message); //just to demonstrate and not for production
+			}
+		});
+
+
 		Message message = MessageBuilder.withPayload("Hello World payload")
 				.setHeader("key", "hello world header value").build();
-		final PrintService service = new PrintService();
-		service.print(message);
+		channel.send(message);
 
 	}
 }
