27b6f746d00a569eca9d4345a154d94d4fe3640e
Khaya Mathebula <khaya.mathebula@absa.co.za>
2018-06-05 21:53:15 +0200

    added @RefreshScope annotation to get updated properties on the fly

--- config-client/src/main/java/com/example/configclient/ConfigClientApplication.java
+++ config-client/src/main/java/com/example/configclient/ConfigClientApplication.java
@@ -2,8 +2,10 @@ package com.example.configclient;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 
 @SpringBootApplication
+@RefreshScope
 public class ConfigClientApplication {
 
 	public static void main(String[] args) {
