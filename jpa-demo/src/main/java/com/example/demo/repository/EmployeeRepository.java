4265dd0f77f5f13c926415354a5070c89dd3a7fb
Khaya Mathebula <khaya.mathebula@absa.co.za>
2018-07-13 12:44:38 +0200

    added function to remove, raise salary and get all employees

--- jpa-demo/src/main/java/com/example/demo/repository/EmployeeRepository.java
+++ jpa-demo/src/main/java/com/example/demo/repository/EmployeeRepository.java
@@ -5,13 +5,19 @@ import org.springframework.stereotype.Service;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
 import javax.transaction.Transactional;
+import java.util.List;
 
 @Service
 public class EmployeeRepository {
 
     @PersistenceContext
     private EntityManager em;
+
+    public EmployeeRepository(final EntityManager em) {
+        this.em = em;
+    }
 
     @Transactional
     public void save(final Employee employee) {
@@ -23,7 +29,26 @@ public class EmployeeRepository {
     }
 
     @Transactional
-    public void removeEmployee(final Employee employee) {
+    public void removeEmployee(final long id) {
+        final Employee employee = findById(id);
+        if(employee == null) {
+            throw new IllegalArgumentException("No employee with id: " + id + " exist");
+        }
         em.remove(employee);
+    }
+
+    @Transactional
+    public Employee raiseEmployeeSalary(final long empId, double percentage) {
+        final Employee employee = em.find(Employee.class, empId);
+        if (employee == null) {
+            throw new IllegalArgumentException("No employee with id: " + empId + " exist");
+        }
+        employee.setSalary(employee.getSalary() + (employee.getSalary() * percentage / 100));
+        return employee;
+    }
+
+    public List<Employee> getAllEmployees() {
+        final TypedQuery<Employee> query = em.createQuery("SELECT emp FROM Employee emp", Employee.class);
+        return query.getResultList();
     }
 }
--- jpa-demo/src/test/java/com/example/demo/repository/EmployeeRepositoryTest.java
+++ jpa-demo/src/test/java/com/example/demo/repository/EmployeeRepositoryTest.java
@@ -7,6 +7,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.List;
+
 import static org.assertj.core.api.Java6Assertions.assertThat;
 
 @RunWith(SpringRunner.class)
@@ -18,23 +20,59 @@ public class EmployeeRepositoryTest {
 
     @Test
     public void should_save_employee() {
-        final Employee employee = new Employee();
-        employee.setName("Sam");
-        employee.setSalary(10000d);
-        repository.save(employee);
+        final Employee employee = saveEmployee();
         assertThat(employee.getId()).isNotNull();
+        assertThat(employee.getName()).isEqualTo("Sam");
     }
 
     @Test
     public void should_save_and_find_employee() {
-        final Employee response = repository.findById(1L);
+        final Employee employee = saveEmployee();
+        final Employee response = repository.findById(employee.getId());
         assertThat(response).isNotNull();
         assertThat(response.getName()).isEqualTo("Sam");
     }
 
     @Test
     public void should_remove_employee() {
+        final Employee employee = saveEmployee();
+       repository.removeEmployee(employee.getId());
+        final Employee response = repository.findById(employee.getId());
+        assertThat(response).isNull();
+    }
+
+    @Test
+    public void should_get_all_employees () {
+        final Employee employee = saveEmployee();
+        final List<Employee> allEmployees = repository.getAllEmployees();
+        allEmployees.forEach(emp -> {
+            assertThat(emp.getName()).isEqualTo(employee.getName());
+            assertThat(emp.getId()).isEqualTo(employee.getId());
+            assertThat(emp.getSalary()).isEqualTo(employee.getSalary());
+        });
+    }
+
+    @Test
+    public void should_raise_employee_salary () {
+        Employee employee = saveEmployee();
+        employee = repository.raiseEmployeeSalary(employee.getId(), 10);
+        assertThat(employee.getSalary()).isEqualTo(11000d);
+        //confirming that the salary change has been persisted successfully
+        final Employee response = repository.findById(employee.getId());
+        assertThat(response.getSalary()).isEqualTo(11000d);
+
+    }
+
+    private static Employee getEmployeeInstance() {
         final Employee employee = new Employee();
-        repository.removeEmployee(employee);
+        employee.setName("Sam");
+        employee.setSalary(10000d);
+        return employee;
+    }
+
+    private Employee saveEmployee() {
+        final Employee employee = getEmployeeInstance();
+        repository.save(employee);
+        return employee;
     }
 }
