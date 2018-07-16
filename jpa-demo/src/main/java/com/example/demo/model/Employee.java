f1257b3b6b16c95d6e863104a866625921587614
Khaya Mathebula <khaya.mathebula@absa.co.za>
2018-07-16 17:49:23 +0200

    added schema to categorize table set

--- jpa-demo/src/main/java/com/example/demo/model/Employee.java
+++ jpa-demo/src/main/java/com/example/demo/model/Employee.java
@@ -6,9 +6,11 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
 @Data
 @Entity
+@Table(name = "employee", schema = "HR")
 public class Employee {
 
     @Id
