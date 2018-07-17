203578a743d8c409352473070bceb755908d0ee7
Khaya Mathebula <khaya.mathebula@absa.co.za>
2018-07-17 17:52:07 +0200

    added a @Lob annotation for large object

--- jpa-demo/src/main/java/com/example/demo/model/Employee.java
+++ jpa-demo/src/main/java/com/example/demo/model/Employee.java
@@ -2,20 +2,27 @@ package com.example.demo.model;
 
 import lombok.Data;
 
+import javax.persistence.Basic;
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Lob;
 import javax.persistence.Table;
 
 @Data
 @Entity
-@Table(name = "employee", schema = "HR")
+@Table(name = "employee")
 public class Employee {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Basic
     private String name;
     private Double salary;
+    @Lob
+    @Column(name = "emp_pic")
+    private byte[] picture;
 }
