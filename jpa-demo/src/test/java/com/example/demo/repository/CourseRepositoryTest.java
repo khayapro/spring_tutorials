7559ec6186b9d254e18e5c4b80f7307f81f2c216
Khaya Mathebula <khaya.mathebula@absa.co.za>
2018-07-19 17:47:18 +0200

    testing my repository, and loading the spring context with my test

--- jpa-demo/src/test/java/com/example/demo/repository/CourseRepositoryTest.java
+++ jpa-demo/src/test/java/com/example/demo/repository/CourseRepositoryTest.java
@@ -1,13 +1,26 @@
 package com.example.demo.repository;
 
+import com.example.demo.DemoApplication;
+import com.example.demo.model.Course;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
-import static org.junit.Assert.*;
+import static org.assertj.core.api.Java6Assertions.assertThat;
 
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = DemoApplication.class)
 public class CourseRepositoryTest {
+
+    @Autowired
+    private CourseRepository repository;
 
     @Test
     public void findById() {
+        final Course response = repository.findById(1L);
+        assertThat(response.getName()).isEqualTo("Learning JPA");
     }
 
     @Test
