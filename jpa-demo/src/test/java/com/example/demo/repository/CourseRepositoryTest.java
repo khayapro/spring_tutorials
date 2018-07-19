2357d6642606f4d5ed42753d064b957e673b5a3a
Khaya Mathebula <khaya.mathebula@absa.co.za>
2018-07-19 17:41:40 +0200

    enabled h2 console and test

---
+++ jpa-demo/src/test/java/com/example/demo/repository/CourseRepositoryTest.java
@@ -0,0 +1,16 @@
+package com.example.demo.repository;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class CourseRepositoryTest {
+
+    @Test
+    public void findById() {
+    }
+
+    @Test
+    public void save() {
+    }
+}
