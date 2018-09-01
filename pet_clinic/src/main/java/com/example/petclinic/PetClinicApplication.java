c092963050a77b5551769e5875cc668bb44aa59b
Khaya Mathebula <khaya.mathebula@absa.co.za>
2018-09-01 12:27:26 +0200

    experimenting with spring di - using qualifiers

--- pet_clinic/src/main/java/com/example/petclinic/PetClinicApplication.java
+++ pet_clinic/src/main/java/com/example/petclinic/PetClinicApplication.java
@@ -1,19 +1,21 @@
 package com.example.petclinic;
 
 import com.example.petclinic.controller.SimpleController;
+import com.example.petclinic.services.PrimaryGreetingService;
+import com.example.petclinic.services.SecondGreetingService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.ConfigurableApplicationContext;
 
 @Slf4j
 @SpringBootApplication
 public class PetClinicApplication {
 
 	public static void main(String[] args) {
-		final ApplicationContext ctx = SpringApplication.run(PetClinicApplication.class, args);
-		final SimpleController bean = ctx.getBean(SimpleController.class);
-		log.warn(bean.hello());
+		final ApplicationContext cxt = SpringApplication.run(PetClinicApplication.class, args);
+		log.warn("***** : {}", cxt.getBean(SimpleController.class).hello());
+		log.warn("***** : {}", cxt.getBean(PrimaryGreetingService.class).greet());
+		log.warn("***** : {}", cxt.getBean(SecondGreetingService.class).greet());
 	}
 }
--- pet_clinic/src/main/java/com/example/petclinic/controller/SimpleController.java
+++ pet_clinic/src/main/java/com/example/petclinic/controller/SimpleController.java
@@ -1,11 +1,21 @@
 package com.example.petclinic.controller;
 
+import com.example.petclinic.interfaces.GreetingService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class SimpleController {
 
+    private GreetingService greetingService;
+
+    @Autowired
+    public SimpleController(@Qualifier("primaryGreetingService") final GreetingService greetingService) {
+        this.greetingService = greetingService;
+    }
+
     public String hello() {
-        return "Hello There from SpringContext";
+        return greetingService.greet();
     }
 }
