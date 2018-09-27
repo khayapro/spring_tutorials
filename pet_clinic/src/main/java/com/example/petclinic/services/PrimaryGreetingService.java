9cb77809710533fd257ff2988081ba816b6d7440
Khaya Mathebula <khaya.mathebula@absa.co.za>
2018-09-27 17:50:15 +0200

    working with profiles in spring

--- pet_clinic/src/main/java/com/example/petclinic/services/PrimaryGreetingService.java
+++ pet_clinic/src/main/java/com/example/petclinic/services/PrimaryGreetingService.java
@@ -2,10 +2,12 @@ package com.example.petclinic.services;
 
 import com.example.petclinic.interfaces.GreetingService;
 import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
 @Service
 @Primary
+@Profile(value = "en")
 public class PrimaryGreetingService implements GreetingService {
 
     @Override
