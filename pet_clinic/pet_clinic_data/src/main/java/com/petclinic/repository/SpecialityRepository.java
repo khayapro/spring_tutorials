64cfde5d5d226f6deefcfc25fd7b1ede12ee7277
Khaya Mathebula <khayaprom@gmail.com>
2019-02-19 19:37:34 +0200

    added speciality repository

---
+++ pet_clinic/pet_clinic_data/src/main/java/com/petclinic/repository/SpecialityRepository.java
@@ -0,0 +1,10 @@
+package com.petclinic.repository;
+
+import com.petclinic.model.Speciality;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface SpecialityRepository extends JpaRepository<Speciality, Long> {
+
+}
