e7aceefc6201a16924a80a068efa6ab9a8a16ef7
Khaya Mathebula <khayaprom@gmail.com>
2020-05-13 19:49:03 +0200

    Added configuration class for bean creation

---
+++ conference-java/src/main/java/AppConfig.java
@@ -0,0 +1,22 @@
+import com.aonsource.com.aonsource.service.SpeakerService;
+import com.aonsource.com.aonsource.service.SpeakerServiceImpl;
+import com.aonsource.repository.HibernateSpeakerRepositoryImpl;
+import com.aonsource.repository.SpeakerRepository;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AppConfig {
+
+    @Bean(name = "speakerRepository")
+    public SpeakerRepository getSpeakerRepository() {
+        return new HibernateSpeakerRepositoryImpl();
+    }
+
+    @Bean(name = "speakerService")
+    public SpeakerService getSpeakerService() {
+        return new SpeakerServiceImpl();
+    }
+
+
+}
