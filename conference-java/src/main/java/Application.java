0342e7ebc2d75959a466efddf30daeb5c19e4cd0
Khaya Mathebula <khayaprom@gmail.com>
2020-05-13 19:58:43 +0200

    refactored and using applicationContext for bean injection

--- conference-java/src/main/java/Application.java
+++ conference-java/src/main/java/Application.java
@@ -1,12 +1,15 @@
 import com.aonsource.com.aonsource.service.SpeakerService;
-import com.aonsource.com.aonsource.service.SpeakerServiceImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class Application {
 
     public static void main(String [] args) {
-        final SpeakerService speakerService = new SpeakerServiceImpl();
-        System.out.println(speakerService.findAll().get(0).getFirstName());
 
+        ApplicationContext appContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        final SpeakerService speakerService = appContext.getBean("speakerService", SpeakerService.class);
+
+        System.out.println(speakerService.findAll().get(0).getFirstName());
     }
 
 }
--- conference-java/src/main/java/com/aonsource/com/aonsource/service/SpeakerServiceImpl.java
+++ conference-java/src/main/java/com/aonsource/com/aonsource/service/SpeakerServiceImpl.java
@@ -1,20 +1,21 @@
 package com.aonsource.com.aonsource.service;
 
 import com.aonsource.model.Speaker;
-import com.aonsource.repository.HibernateSpeakerRepositoryImpl;
 import com.aonsource.repository.SpeakerRepository;
+import lombok.Setter;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.List;
 
+@Setter
 public class SpeakerServiceImpl implements SpeakerService {
 
-    private SpeakerRepository repository = new HibernateSpeakerRepositoryImpl();
+    @Autowired
+    private SpeakerRepository repository;
 
     @Override
     public List<Speaker> findAll() {
         return repository.findAll();
     }
-
-
 
 }
