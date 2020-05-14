d64298678fc826943d29e57c647148a79571a4ea
Khaya Mathebula <khayaprom@gmail.com>
2020-05-14 22:57:28 +0200

    init conference project

---
+++ conference-demo/src/main/java/za/co/aonsource/conferencedemo/ConferenceDemoApplication.java
@@ -0,0 +1,13 @@
+package za.co.aonsource.conferencedemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConferenceDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConferenceDemoApplication.class, args);
+    }
+
+}
---
+++ conference-demo/src/test/java/za/co/aonsource/conferencedemo/ConferenceDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package za.co.aonsource.conferencedemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConferenceDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
